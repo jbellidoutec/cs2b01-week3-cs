2aaceab5d96255ad8b5d9eaf9ae8604c5ff2b3b2
jesubellido <jesubellido@gmail.com>
2021-05-14 18:53:45 -0500

    week 4

--- web/server.py
+++ web/server.py
@@ -10,8 +10,23 @@ app = Flask(__name__)
 
 @app.route('/saludar')
 def saludar():
-    return render_template('/static/html/index.html')
+    return "<h1>HOLA</h1>"
 
+
+@app.route("/sumar/<n1>/<n2>")
+def sumar(n1, n2):
+    return str(int(n1)+int(n2))
+
+@app.route('/authenticate', methods = ['POST'])
+def authenticate():
+    #Get data form request
+    message = json.loads(request.data)
+    username = message['username']
+    password = message['password']
+    print(username, password)
+    if username == password:
+        return json.dumps({"message":"si"})
+    return json.dumps({"message":"fail"})
 
 if __name__ == '__main__':
     app.secret_key = ".."
